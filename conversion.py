027682907a1861e92b246a969cc3838074f044ad
AbdulMajeed <abdomonsar@gmail.com>
2026-04-20 22:39:06 +0300

    Last sentence

--- conversion.py
+++ conversion.py
@@ -1,46 +1,46 @@
-# cal=(input("Enter String: "))
-# print(type(cal))
-# cal=(int(cal))
-# print(type(cal))
-# st=input("Enter Float:")
-# print(type(st))
-# st=float(st)
-# print(type(st))
-# integer=int(input("Enter number: "))
-# print(type(integer))
-# integer=str(integer)
-# print(type(integer))
-# bol=int(input("Enter Integer: "))
-# print(type(bol))
-# bol=bool(bol)
-# print(type(bol))
-# word=["Hello","World","Python"]
-# result=" ".join(word)
-# print(result)
-# text = "Hello World Python"
-# result = text.split()
-# print(result)
-# number=[1,2,3,4,2,3]
-# number=set(number)
-# print(number)
-# ch={1,2,3,4,5}
-# ch=list(ch)
-# print(ch)
-# num=66.8
-# num=int(num)
-# print(num)
-# print(int(True))
-# text = "Hello"
-# bytes_data = text.encode("UTF-8")
+cal=(input("Enter String: "))
+print(type(cal))
+cal=(int(cal))
+print(type(cal))
+st=input("Enter Float:")
+print(type(st))
+st=float(st)
+print(type(st))
+integer=int(input("Enter number: "))
+print(type(integer))
+integer=str(integer)
+print(type(integer))
+bol=int(input("Enter Integer: "))
+print(type(bol))
+bol=bool(bol)
+print(type(bol))
+word=["Hello","World","Python"]
+result=" ".join(word)
+print(result)
+text = "Hello World Python"
+result = text.split()
+print(result)
+number=[1,2,3,4,2,3]
+number=set(number)
+print(number)
+ch={1,2,3,4,5}
+ch=list(ch)
+print(ch)
+num=66.8
+num=int(num)
+print(num)
+print(int(True))
+text = "Hello"
+bytes_data = text.encode("UTF-8")
 
-# print(bytes_data)
-# bytes_data=bytes_data.decode()
-# print(bytes_data)
-# num=90
-# num2=789
-# num=bin(num)
-# num=hex(num2)
-# print(num)
-# print(num2)
-# print(ord("A"))
-# print(chr(56))
+print(bytes_data)
+bytes_data=bytes_data.decode()
+print(bytes_data)
+num=90
+num2=789
+num=bin(num)
+num=hex(num2)
+print(num)
+print(num2)
+print(ord("A"))
+print(chr(56))
