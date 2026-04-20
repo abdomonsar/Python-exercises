5d37ad70712141f9b24904c82d4d3068d97c3ecb
AbdulMajeed <abdomonsar@gmail.com>
2026-04-20 17:47:51 +0300

    Last sentence

---
+++ Python exercises/basics.py
@@ -0,0 +1,44 @@
+# 1
+# print("Hello World")
+#2
+# name=input("Enter Your Name: ")
+# print("Welcom"+" "+name)
+#3
+# name=input("Enter Your Name: ")
+# age=int(input("Enter Your Age: "))
+# city=input("Enter Your City: ")
+# print(f"Your Name:{name} Your Age:{age} Your City:{city}")
+#4
+# number=int(input("Enter Number: "))
+# print(number)
+#5
+# number=float(input("Enter Number: "))
+# print(number)
+#6
+# bo=input("Enter True Or False: ")
+# print(type(bo))
+# bo=bool(bo)
+# print(bo,type(bo))
+#7
+# number=int(input("Enter Number: "))
+# st=input("Enter String: ")
+# fl=float(input("Enter Float: "))
+# print(F"{number} {st} {fl}")
+#8
+# print("AbdulMajeed","Abdo","Saeed",sep="_")
+#9
+# print("Hello",end=" ")
+# print("world")
+#10
+# print("To end the entry, type end")
+# lines=[]
+# while True:
+#     line=input()
+#     if line=="end":
+#         break
+#     lines.append(line)
+# for i in lines:
+#     print(i)    
+print("The first part has been completed")
+
+     
