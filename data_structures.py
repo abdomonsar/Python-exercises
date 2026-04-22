429c419e5ace8a14fea62912ce83a37605a0860d
AbdulMajeed <abdomonsar@gmail.com>
2026-04-22 10:26:06 +0300

    frist

--- data_structures.py
+++ data_structures.py
@@ -1,1 +1,20 @@
-print("Hello")
+#Create an empty list and add items to it:
+# my_list=[]
+# co=int(input("Enter Number list: "))
+# for i in range(co):
+#     item=input(f"Enter item {i+1}")
+#     my_list.append(item)
+# print(my_list)    
+#Accessing menu items
+# name=["A","B","C"]
+# print(name[0])
+# print(name[-1])
+#Modify an item in a list
+# fruits = ["Apple", "Banana", "Orange"]
+# fruits[2]="Mango"
+# print(fruits)
+#Delete an item from a list
+# fruits = ["Apple", "Banana", "Orange"]
+# fruits.remove("Apple")
+# print(fruits)
+
