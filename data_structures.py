4da872c5ade1d290bbe4ff2513e491ebeefacaf9
AbdulMajeed <abdomonsar@gmail.com>
2026-04-22 15:27:34 +0300

    Done!

--- data_structures.py
+++ data_structures.py
@@ -1,53 +1,60 @@
-#Create an empty list and add items to it:
+# #Create an empty list and add items to it:
 my_list=[]
 co=int(input("Enter Number list: "))
 for i in range(co):
     item=input(f"Enter item {i+1}")
     my_list.append(item)
 print(my_list)    
-#Accessing menu items
+# #Accessing menu items
 name=["A","B","C"]
 print(name[0])
 print(name[-1])
-#Modify an item in a list
+# #Modify an item in a list
 fruits = ["Apple", "Banana", "Orange"]
 fruits[2]="Mango"
 print(fruits)
-#Delete an item from a list
+# #Delete an item from a list
 fruits = ["Apple", "Banana", "Orange"]
 fruits.remove("Apple")
 print(fruits)
-#Merge two lists
+# #Merge two lists
 list1 = ["Apple", "Banana"]
 list2 = ["Orange", "Mango"]
 mar=list1 + list2
 print(mar)
-#Sort a list of numbers
+# #Sort a list of numbers
 numbers = [5, 2, 9, 1, 7]
 numbers.sort()
 print(numbers)
 numbers.sort(reverse=True)
 print(numbers)
-#Find the largest and smallest item in a list (using built-in functions):
+# #Find the largest and smallest item in a list (using built-in functions):
 numbers = [5, 2, 9, 1, 7]
 print(max(numbers))
 print(min(numbers))
-#No duplicates of an item in a list
+# #No duplicates of an item in a list
 count=0
 numbers = [5, 2, 2, 1, 7]
 for i in numbers:
     if i==2:
         count+=1
 print(count)        
-#Create a submenu (Slicing):
+# #Create a submenu (Slicing):
 menu = ["Burger", "Pizza", "Juice", "Salad", "Fries", "Water"]
 sub=menu[1:4]
 print(sub)
-#Remove an item from a specific location in a list:
+# #Remove an item from a specific location in a list:
 my_list = ["Apple", "Banana", "Orange", "Mango"]
 my_list.pop(2)
 print(my_list)
-#Clear all list items:
+# #Clear all list items:
 my_list = ["Apple", "Banana", "Orange", "Mango"]
 my_list.clear()
 print(my_list)
+#Create a tuple and access its elements
+my_tuple = ("Apple", "Banana", "Orange", "Mango")
+print(my_tuple[1])
+#Merge two rows
+row1=(1,2,3)
+row2=(4,2,7)
+print(row1 +row2)
